1e24ede34d95ee4847fb2db172af276f6e5570db
Newton <newtieblackie@gmail>
2022-07-26 16:58:45 +0300

    NavBar component feeded

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function NavBar() {
     return (
-        <div className="navbar"></div>
+        <div className="navbar">
+            <a href="#" className="App-logo"><img src="src/Assets/Essesntials/Studio_Project.svg"></img></a>
+            <div className="navbar-inner">
+                <a href="#">Home</a>
+                <a href="#">Book</a>
+                <a href="#">Gallery</a>
+            </div>
+            <form className="search-bar-container">
+                <input type="text" id="search-bar-input" placeholder="Search"></input>
+            </form>
+        </div>
     );
 }
