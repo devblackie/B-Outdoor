34b770de511540d7e4e40ec9324b4516b1186708
Newton <newtieblackie@gmail>
2022-07-26 21:20:16 +0300

    Defined AboutUs component

---
+++ src/components/AboutUs.js
@@ -0,0 +1,49 @@
+<div className="contact">
+
+	<h1 className="heading">
+		<span>C</span>
+		<span>O</span>
+		<span>N</span>
+		<span>T</span>
+		<span>A</span>
+		<span>C</span>
+		<span>T</span>
+	</h1>
+
+	<div className="Book">
+
+		<div className="image">
+			<img src=""/>
+		</div>
+
+		<form>
+		   <div className="form-input">
+		   	 <label>Name</label>
+		   	 <input type="text" placeholder="Name"/>
+		   	 
+		   	 <label>Email</label>
+		   	 <input type="email" placeholder="Email"/>
+		   	 
+		   </div>
+		   <div className="form-input">
+		   	 <label>Phone Number</label>
+		   	 <input type="tel" placeholder="Number"/>
+		   	 
+		   	 <label>Subject</label>
+		   	 <input type="text" placeholder="Subject" name="Subject"/>
+		   	 
+		   </div>
+		   <label>Message</label>
+		   <textarea placeholder="Message" name="Message" cols="30" rows="10"></textarea>
+		   
+		   <button type="submit" class="btn" style="border-radius: .5rem">Send Message</button>
+	   </form>
+
+		
+	</div>
+
+	
+
+	
+
+</div>
--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,7 @@ function App() {
       <Home/>
       <Book/>
       <Gallery/>
+      <AboutUs/>
       <Footer/>
     </div>
   );
